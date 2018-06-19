3afb667499b7af33799fd996a7e248b6e80c3631
Reza Rajan <rezarajan@gmail.com>
2018-06-19 06:31:58 -0400

    Created functions to scrape data and an object to store it in Data manipulation occurs on the object IMDB example used here Object manipulation is flexible, and based on the data source

---
+++ node_scraper.js
@@ -0,0 +1,80 @@
+var express = require('express');
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+var app = express();
+
+/* Parse fetched data */
+function parseHTML(html){
+    let $ = cheerio.load(html);
+    return $;
+}
+/* Fetch data from source */
+function scraper(url) {
+    return new Promise(function(resolve, reject) {
+        if (!url) {
+            return reject(new Error("URL must be specified"));
+        }
+
+        request(url, function(err, res, html) {
+            if (err) {
+                return reject(err);
+            } else {
+                let urlInfo = parseHTML(html);
+                if (!urlInfo)
+                    return reject(new Error("Invalid URL"));
+                    // console.log(urlInfo)
+                resolve(urlInfo);
+            }
+        });
+    });
+}
+/* Object for scraped Data */
+class ScrapedData {
+    constructor(url, info={}) {
+        /* Overwrites default ad properties */
+        let overwriteProps = (info) => {
+            this.info = info;
+        }
+
+        // Overwrite with passed info
+        overwriteProps(info);
+    }
+
+    /* Creates an object by scraping the passed URL */
+    static Get(url, callback) {
+        let promise = scraper(url).then(function(info) {
+            return new ScrapedData(url, info, true);
+        });
+
+        if (callback)
+            promise.then((ad) => callback(null, ad), callback);
+        return promise;
+    }
+}
+
+
+/* Data Sources */
+url = 
+{
+    kijiji: "https://www.kijiji.ca/b-real-estate/kitchener-waterloo/apartment/k0c34l1700212", 
+    imdb: "https://www.imdb.com/title/tt1229340/"
+};
+
+/* Manipulate data */
+ScrapedData.Get(url.imdb).then(function(rawdata) {
+    // Use the scraped data object
+    let scrapedData = rawdata.info;
+    scrapedData('.title_wrapper').filter(function(){
+
+                var data = scrapedData(this);
+
+                title = data.children().first().text();
+                console.log(data.children().first().text());
+        })
+}).catch(console.error);
+
+module.exports = 
+{
+    app
+}
